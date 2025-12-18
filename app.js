28a5753a894d8945637e5f1ef52f17288dd0a5c3
zakriya <zalla8762@gmail.com>
2025-12-18 09:43:55 +0100

    Connect to TiDB Cloud database

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
+    ca: fs.readFileSync(path.join(__dirname, process.env.ca)),
     minVersion: "TLSv1.2",
     rejectUnauthorized: true,
   },
